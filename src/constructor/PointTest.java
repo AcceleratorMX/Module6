d625289b221a1e41e00171a30b631646a23770e7
AcceleratorMX <redfield242@gmail.com>
2022-11-15 11:51:26 +0200

    first

---
+++ src/constructor/PointTest.java
@@ -0,0 +1,11 @@
+package src.constructor;
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point point = new Point();
+        point.setX(1);
+        point.setY(10);
+        point.setZ(100);
+        System.out.println("point = " + point);
+    }
+}
