82174a0e8cbfb93ba712639f81570b0d3a6c02ef
AcceleratorMX <redfield242@gmail.com>
2022-11-19 00:56:46 +0200

    first

---
+++ src/HomeWork/h6/Bullet.java
@@ -0,0 +1,15 @@
+package src.HomeWork.h6;
+
+class Bullet {
+
+    {
+        System.out.println("Bullet created!");
+    }
+}
+
+class BulletTest {
+    public static void main(String[] args) {
+        new Bullet();
+        new Bullet();
+    }
+}
